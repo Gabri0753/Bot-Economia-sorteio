6c9b74bb1b6b00e31e4b6b4696ca6dca1d6c5132
Gabri0753 <104369233+Gabri0753@users.noreply.github.com>
2022-07-31 20:30:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
-const Client = require("./src/index.js")
+// Packages and Variables
+const Discord = require("discord.js");
+const moment = require("moment")
+require("moment-duration-format")
+const { QuickDB } = require("quick.db");
+const db = new QuickDB();
+const { InteractionType } = require("discord-api-types/v10")
 
-module.exports = Client;
+
+// Getting online
+const client = new Discord.Client({ intents: 131071, shards: "auto" });
+client.login("TOKEN DO SEU BOT");
+
+// Slash Commands
+module.exports = client;
+client.commands = new Discord.Collection();
+client.slashCommands = new Discord.Collection();
+require("./handler")(client);
+const { glob } = require("glob");
+const { promisify } = require("util");
+
+const globPromise = promisify(glob);
+
+client.on("interactionCreate", async (interaction) => {
+  
+  if(!interaction.guild) return interaction.reply({content: `❌ infelizmente os comandos em slash não pode ser executados na minha dm.`, ephemeral: true});
+
+ if(interaction.type == 2) {
+   const cmd = client.slashCommands.get(interaction.commandName)
+if(!cmd)
+ return;
+
+ const args = []
+
+   for(let option of interaction.options.data) {
+     if(option.type === "SUB_COMMAND") {
+       if(option.name) args.push(option.value);
+       option.options?.forEach((x) => {
+         if(x.value) args.push(x.value)
+       })
+     } else if(option.value) args.push(option.value)
+   }
+
+   cmd.run(client, interaction, args);
+ };
+
+});
+
+client.on("ready", () => {
+  console.log("✅ logado em "+client.user.tag)
+}) 
